6da42e05e148786ebaa4446beab6b1250270fad2
priyadharshinig17 <74023961+priyadharshinig17@users.noreply.github.com>
2021-02-16 13:25:01 +0100

    Delete src directory

--- src/__tests__/GitHubUserRepos.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-
-import GitHubUserRepos from '../GitHubUserRepos';
-
-it('renders without crashing', () => {
-  shallow(<GitHubUserRepos />);
-});
